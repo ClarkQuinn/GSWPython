1a58c093945c97e80fddcab464101dba8bf66f19
ClarkQuinn <neuroml63@gmail.com>
2025-06-19 07:50:41 +0530

    GSWPython base notes

---
+++ gswpython.py
@@ -0,0 +1,10 @@
+#this is getting started with python.
+
+import sys
+
+name = 'twisha'
+welcome_message = f"Welcome {name.title()}!"
+print(welcome_message)
+print("You are working on Python version : " + sys.version)
+print("Hello There!")
+print('Tip of the day :  "Live the life through to see if the dots connect" ')
